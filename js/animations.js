d43fe7295c19c8e7789fe34ac82d83a143eec92b
Santiago Barliza Lizcano <santiagobarliza6@gmail.com>
2025-08-15 14:37:47 -0500

    Mensaje envio correcto, y error. (Formulario.)

--- js/animations.js
+++ js/animations.js
@@ -9,3 +9,28 @@ btnRight.addEventListener("click", () => {
 btnLeft.addEventListener("click", () => {
   slidesContainer.scrollBy({ left: -300, behavior: "smooth" });
 });
+
+document.querySelector(".formulario_contacto").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  let campos = document.querySelectorAll(".campo input, .campo textarea");
+  let valido = true;
+
+  campos.forEach(campo => {
+    if (campo.value.trim() === "") {
+      campo.style.border = "2px solid red";
+      valido = false;
+    } else {
+      campo.style.border = "2px solid green";
+    }
+  });
+
+  if (valido) {
+    alert("Formulario enviado correctamente ✅");
+    this.reset();
+  } else {
+    alert("Por favor completa todos los campos ❌");
+  }
+});
+
+
