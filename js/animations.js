768cdcebc4852de9126a40a1c418c7007b220f57
Santiago Barliza Lizcano <santiagobarliza6@gmail.com>
2025-09-25 15:42:13 -0500

    Duración de las cards, pendiente ajustar click

--- js/animations.js
+++ js/animations.js
@@ -61,11 +61,11 @@ const btnLeft = document.querySelector(".btn-carrusel.left");
 const btnRight = document.querySelector(".btn-carrusel.right");
 
 btnRight.addEventListener("click", () => {
-  slidesContainer.scrollBy({ left: 300, behavior: "smooth" });
+  slidesContainer.scrollBy({ left: 1210, behavior: "smooth" });
 });
 
 btnLeft.addEventListener("click", () => {
-  slidesContainer.scrollBy({ left: -300, behavior: "smooth" });
+  slidesContainer.scrollBy({ left: -1210, behavior: "smooth" });
 });
 
 // ------------------------------------------------------ ENVIO FORMULARIO --------------------------------------- -- //
